d1a5bd272e9d12661f63794feee3bcb4f60783e5
REDMOND\v-williamco <williamdouglasgomes@hotmail.com>
2024-12-22 20:02:51 -0300

    Iniciando curso

---
+++ class3/index.js
@@ -0,0 +1,20 @@
+/*let nome = 'William Douglas'
+console.log(`Hello world ${nome}`)
+
+
+let secondName = 'William Douglas'
+
+console.log(`Meu nome é ${secondName}. Estou aprendendo JavaScript às 16:00 da tarde.`)*/
+
+let test = 4
+console.log(typeof(test))
+
+test = 'dijidsjids'
+console.log(typeof(test))
+
+test = false
+console.log(typeof(test))
+
+test = []
+console.log(typeof(test))
+
